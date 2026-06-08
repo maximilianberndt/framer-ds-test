eaaaf046bd57f6fb1564aa47e5489e57e27b36fe
maximilianberndt <berndt.maximilian@gmail.com>
2026-06-08 16:36:02 +0200

    Publish Framer lib build to dist/

---
+++ dist/styles.js
@@ -0,0 +1,6 @@
+import "./style.css";
+
+//#region src/styles.js
+var e = "https://cdn.jsdelivr.net/gh/maximilianberndt/framer-ds-test@main/dist/style.css";
+//#endregion
+export { e as STYLE_URL };
